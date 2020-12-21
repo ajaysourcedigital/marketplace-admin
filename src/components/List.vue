303a942936a5f570a434463930762ce93f8185d3
c-49 <reithc4@gmail.com>
2020-12-21 17:40:26 -0600

    Overwriting current list with q-resources

--- src/components/List.vue
+++ src/components/List.vue
@@ -1,154 +1,178 @@
 <template>
-  <q-card class="q-mt-sm">
-    <q-card-section class="text-h6 q-pb-none">
-      <q-item>
+  <q-card
+    class="q-mt-sm"
+    v-if='fullData'
+  >
+    <q-card-section class="text-h6 q-pb-none row no-wrap items-baseline">
+      <q-item class='col-8'>
         <q-item-section
           avatar
           class=""
         >
           <q-icon
             color="blue"
-            :name="settings.icon"
+            :name="fullData.icon"
             size="44px"
           />
         </q-item-section>
         <q-item-section>
           <q-item-label>
             <div class="text-h6">
-              {{ settings.header }}
+              {{ fullData.header }}
             </div>
           </q-item-label>
           <q-item-label
             caption
             class="text-black"
           >
-            {{ settings.subheader }}
+            {{ fullData.subheader }}
           </q-item-label>
         </q-item-section>
       </q-item>
+      <div class='col-4 row justify-end'>
+        <div>
+          <q-btn
+            round
+            flat
+            :icon='card ? "list" : "view_module"'
+            @click='card = !card'
+          />
+        </div>
+      </div>
     </q-card-section>
     <q-card-section class="q-pa-none q-ma-none">
       <q-table
-        class="no-shadow no-border"
-        :data="settings.data"
-        :columns="settings.columns"
+        card-container-style='height:400px;overflow:scroll;'
+        table-style="height: 400px;"
+        card-style='max-height:400px;'
+        class='stickyTable'
+        :data="validation"
+        :grid='card'
+        row-key="name"
         hide-bottom
+        virtual-scroll
+        :rows-per-page-options="[0]"
+        table-colspan="6"
       >
-        <template v-slot:body-cell-Products="props">
-          <q-td :props="props">
-            <q-item>
-              <q-item-section>
-                <q-avatar square>
-                  <img :src="props.row.prod_img">
-                </q-avatar>
-              </q-item-section>
-
-              <q-item-section>
-                <q-item-label>{{ props.row.code }}</q-item-label>
-                <q-item-label>{{ props.row.product_name }}</q-item-label>
-              </q-item-section>
-            </q-item>
-          </q-td>
+        <!-- Default | List view -->
+        <template
+          v-slot:header="props"
+          v-if='!card'
+        >
+          <q-tr :props="props">
+            <q-th />
+            <q-th
+              v-for="col in props.cols"
+              :key="col.name"
+              :props="props"
+            >
+              {{ col.label }}
+            </q-th>
+          </q-tr>
         </template>
-        <template v-slot:body-cell-Name="props">
-          <q-td :props="props">
-            <q-item>
-              <q-item-section avatar />
-
-              <q-item-section>
-                <q-item-label>{{ props.row.name }}</q-item-label>
-                <q-item-label
-                  caption
-                  class=""
-                >
-                  Joined date: <br>{{ props.row.date }}
-                </q-item-label>
-              </q-item-section>
-            </q-item>
-          </q-td>
-        </template>
-        <template v-slot:body-cell-Status="props">
-          <q-td
+        <template v-slot:body="props">
+          <q-tr
             :props="props"
-            class="text-left"
+            class='cursor-pointer'
+            auto-width
           >
-            <q-chip
-              class="text-white text-capitalize"
-              :label="props.row.status"
-              :color="getChipColor(props.row.status)"
-            />
-          </q-td>
-        </template>
-        <template v-slot:body-cell-Stock="props">
-          <q-td :props="props">
-            <q-item>
-              <q-item-section>
-                <q-item-label>
-                  <span class="text-blue">
-                    <q-icon
-                      name="bug_report"
-                      color="blue"
-                      size="20px"
-                      v-if="props.row.type=='error'"
-                    />
-                    <q-icon
-                      name="settings"
-                      color="blue"
-                      size="20px"
-                      v-if="props.row.type=='info'"
-                    />
-                    <q-icon
-                      name="flag"
-                      color="blue"
-                      size="20px"
-                      v-if="props.row.type=='success'"
-                    />
-                    <q-icon
-                      name="fireplace"
-                      color="blue"
-                      size="20px"
-                      v-if="props.row.type=='warning'"
-                    />
-                    {{ props.row.stock }}
-                  </span>
-                  <q-chip
-                    class="float-right text-white text-capitalize"
-                    :label="props.row.type"
-                    color="positive"
-                    v-if="props.row.type=='success'"
-                  />
-                  <q-chip
-                    class="float-right text-white text-capitalize"
-                    :label="props.row.type"
-                    color="info"
-                    v-if="props.row.type=='info'"
-                  />
-                  <q-chip
-                    class="float-right text-white text-capitalize"
-                    :label="props.row.type"
-                    color="warning"
-                    v-if="props.row.type=='warning'"
-                  />
-                  <q-chip
-                    class="float-right text-white text-capitalize"
-                    :label="props.row.type"
-                    color="negative"
-                    v-if="props.row.type=='error'"
-                  />
-                </q-item-label>
-                <q-item-label
-                  caption
-                  class=""
+            <q-td>
+              <q-avatar
+                square
+                size="xl"
+              >
+                <q-img
+                  :ratio='1'
+                  :src='props.row.image ? props.row.image : "https://via.placeholder.com/150?text=N/A"'
+                />
+              </q-avatar>
+            </q-td>
+            <q-td
+              v-for="col in props.cols"
+              :key="col.name"
+              :props="props"
+              style='max-width:200px;'
+            >
+              <div
+                class='ellipsis'
+                v-if='typeItem(col.value)'
+              >
+                {{ col.value || 'N/A' }}
+                <q-popup-edit
+                  v-if='edit'
+                  auto-save
+                  v-model="props.row[col.name]"
+                  @save='$emit("saved", props.row[col.name], props.row, col.label)'
                 >
-                  <q-linear-progress
-                    dark
-                    :color="getColor(props.row.Progress)"
-                    :value="props.row.Progress/100"
+                  <editInPlace
+                    :props='props'
+                    :col='col'
                   />
-                </q-item-label>
-              </q-item-section>
-            </q-item>
-          </q-td>
+                </q-popup-edit>
+              </div>
+              <div v-else>
+                <!-- If object has more than 3 items -->
+                <!-- <q-tr v-show="props.expand" :props="props">
+              <q-td colspan="100%">
+                <div class="text-left">This is expand slot for row above: {{ props.row.name }}.</div>
+              </q-td>
+            </q-tr> -->
+              </div>
+            </q-td>
+          </q-tr>
+        </template>
+        <!-- Card view -->
+        <template
+          v-slot:item="props"
+          v-if='card'
+        >
+          <div class='q-pa-xs col-xs-12 col-sm-6 col-md-4 col-lg-3'>
+            <q-card>
+              <div
+                class='q-pa-md'
+                v-if='props.row.image'
+              >
+                <q-img
+                  :ratio='16/9'
+                  :src='props.row.image'
+                />
+              </div>
+              <q-list
+                dense
+                class='scroll'
+                style='max-height:300px;'
+              >
+                <q-item
+                  v-for="col in props.cols"
+                  :key="col.name"
+                >
+                  <q-item-section>
+                    <q-item-label>{{ col.label }}</q-item-label>
+                  </q-item-section>
+                  <q-item-section side>
+                    <q-item-label
+                      caption
+                      lines='1'
+                      style='max-width:150px;'
+                    >
+                      {{ col.value || 'N/A' }}
+                      <q-popup-edit
+                        v-if='edit'
+                        auto-save
+                        v-model="props.row[col.name]"
+                        @save='$emit("saved", props.row[col.name], props.row, col.label)'
+                      >
+                        <editInPlace
+                          :props='props'
+                          :col='col'
+                        />
+                      </q-popup-edit>
+                    </q-item-label>
+                  </q-item-section>
+                </q-item>
+              </q-list>
+            </q-card>
+          </div>
         </template>
       </q-table>
     </q-card-section>
@@ -156,33 +180,69 @@
 </template>
 
 <script>
+const Ajv = require('ajv')
+const ajv = new Ajv({ allErrors: true })
+import editInPlace from './EditInPlace'
 export default {
-  name: 'List',
+  name: 'QResources',
   props: {
-    settings: Object
+    data: { type: Array, default: null },
+    schema: { type: Object, default: null },
+    edit: { type: Boolean, default: false },
+    sticky: { type: Boolean, default: true },
+    fullData: { type: Object, default: null }
+  },
+  components: { editInPlace },
+  data () {
+    return {
+      card: false
+    }
   },
   methods: {
-    getColor (val) {
-      if (val > 70 && val <= 100) {
-        return 'green'
-      } else if (val > 50 && val <= 70) {
-        return 'blue'
-      }
-      return 'red'
+    typeItem (e) {
+      if (typeof e === 'object' && e !== null && Object.keys(e).length < 3) return true
+      else if (typeof e !== 'object') return true
+      else if (e === null || e === '') return true
+      else return false
+    }
+  },
+  computed: {
+    validate () {
+      return ajv.compile(this.schema)
     },
-    getChipColor (status) {
-      if (status === 'Vacation') {
-        return 'negative'
-      } else if (status === 'Online') {
-        return 'positive'
-      } else if (status === 'Away') {
-        return 'warning'
-      } else if (status === 'Logged Out') {
-        return 'info'
-      } else {
-        return 'dark'
-      }
+    // validate items and pushes them to a list
+    validation () {
+      const list = []
+      this.data.forEach(item => {
+        const valid = this.validate(item)
+        if (!valid) this.$emit('error', this.validate.errors, item)
+        else list.push(item)
+      })
+      return list
     }
   }
 }
 </script>
+
+<style lang="stylus" scoped>
+.stickyTable {
+  .q-table__top, .q-table__bottom, thead tr:first-child th { /* bg color is important for th; just specify one */
+    background-color: #fff;
+  }
+
+  thead tr th {
+    position: sticky;
+    z-index: 1;
+  }
+
+  /* this will be the loading indicator */
+  thead tr:last-child th {
+    /* height of all previous header rows */
+    top: 48px;
+  }
+
+  thead tr:first-child th {
+    top: 0;
+  }
+}
+</style>
