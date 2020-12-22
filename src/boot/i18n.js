d4a4b2cf4f803cca4093f74e9d1e779e5f0b40fa
sonokoh <74166775+sonokoh@users.noreply.github.com>
2020-12-22 14:58:43 -0500

    add: support for i18n dateTimeFormats

--- src/boot/i18n.js
+++ src/boot/i18n.js
@@ -1,13 +1,15 @@
 import Vue from 'vue'
 import VueI18n from 'vue-i18n'
-import messages from 'src/i18n'
+import messages from 'src/i18n/messages'
+import dateTimeFormats from 'src/i18n/dateTimeFormats'
 
 Vue.use(VueI18n)
 
 const i18n = new VueI18n({
   locale: 'en-us',
   fallbackLocale: 'en-us',
-  messages
+  messages,
+  dateTimeFormats
 })
 
 export default ({ app }) => {
---
+++ src/i18n/dateTimeFormats/en-us/index.js
@@ -0,0 +1,14 @@
+// This is just an example,
+// so you can safely delete all default props below
+
+export default {
+  short: { dateStyle: 'medium' },
+  long: {
+    year: 'numeric',
+    month: '2-digit',
+    day: '2-digit',
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit'
+  }
+}
