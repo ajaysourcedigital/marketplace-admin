01f449f11febe9a0ed6be81fafe1f7858ef0a308
sonokoh <74166775+sonokoh@users.noreply.github.com>
2020-12-22 14:57:42 -0500

    update: use stylus

--- quasar.conf.js
+++ quasar.conf.js
@@ -21,7 +21,7 @@ module.exports = function (ctx) {
 
     // https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-css
     css: [
-      'app.css'
+      'app.styl'
     ],
 
     // https://github.com/quasarframework/quasar/tree/dev/extras
