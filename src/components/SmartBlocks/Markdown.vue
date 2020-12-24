4f300565ed9ae1addf4203a3be11079518b28d84
Nick Steele <njsteele@gmail.com>
2020-12-23 21:27:07 -0500

    Making the markdown editor work

--- src/components/SmartBlocks/Markdown.vue
+++ src/components/SmartBlocks/Markdown.vue
@@ -4,15 +4,21 @@
       v-if="configure"
       class="q-pa-sm"
     >
-      Configure this.
+      This component uses <a href="https://stackedit.io/">Markdown</a> to render
+      content. Type or paste your markdown in the box below.
       <q-input
         label="Content"
-        :value="settings.content"
+        v-model="config.content"
+        filled
+        autogrow
+        @input="emitChange"
+        @blur="emitConfig"
       />
     </div>
-    <div v-else>
-      Your Markdown goes here.
-    </div>
+    <div
+      v-else
+      v-html="html"
+    />
   </div>
 </template>
 <script>
@@ -22,14 +28,30 @@ export default {
     settings: Object,
     configure: Boolean
   },
+  data () {
+    return {
+      // Local copy of settings that can be modified
+      config: this.settings
+    }
+  },
+  computed: {
+    html () {
+      // Process the markdown after we escape the html tags...
+      return this.$ee.templates.md.default(this.$ee.templates.html.escape(this.settings.content))
+    }
+  },
   methods: {
     emitAction () {
+      this.debug('action', this.config)
       this.$emit('action', 'Clicked')
     },
-    emitConfig (data) {
-      const emit = JSON.parse(JSON.stringify(this.settings))
-      emit.image = data
-      this.$emit('config', JSON.stringify(emit))
+    emitConfig () {
+      this.debug('config', this.config)
+      this.$emit('config', this.config)
+    },
+    emitChange (data) {
+      this.debug('change', this.config)
+      this.$emit('change', this.config)
     }
   }
 }
