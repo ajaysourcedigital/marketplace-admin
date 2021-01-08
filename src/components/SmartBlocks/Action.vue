3b996f1787b8772595263754b9a8e62c7bb37e9f
Marta Klaptiuk <tusyvfhnf@gmail.com>
2021-01-08 16:14:11 +0200

    Refactoring Action smartblock

--- src/components/SmartBlocks/Action.vue
+++ src/components/SmartBlocks/Action.vue
@@ -5,61 +5,59 @@
       class="q-pa-sm"
     >
       Customize your action button.
-      <q-input
-        label="Title"
-        ref="input"
-        v-model="settings.title"
-      />
-      <q-input
-        label="URL"
-        v-model="settings.url"
-      />
-      <q-input
-        label="Color"
-        v-model="settings.color"
+      <q-form
+        @submit="emitConfig"
+        @reset="reset"
+        class="q-gutter-sm q-pa-md"
       >
-        <template v-slot:append>
-          <q-icon
-            name="colorize"
-            class="cursor-pointer"
+        <div
+          v-for="con in conf"
+          :key="con.label"
+        >
+          <q-input
+            :label="con.label"
+            v-model="temp[con.model]"
+            clearable
+            clear-icon="close"
+            type="text"
           >
-            <q-popup-proxy
-              transition-show="scale"
-              transition-hide="scale"
+            <!-- Color Picker -->
+            <template
+              v-if="con.colorPicker"
+              v-slot:append
             >
-              <q-color v-model="settings.color" />
-            </q-popup-proxy>
-          </q-icon>
-        </template>
-      </q-input>
-      <q-input
-        label="Text Color"
-        v-model="settings.textColor"
-      >
-        <template v-slot:append>
-          <q-icon
-            name="colorize"
-            class="cursor-pointer"
-          >
-            <q-popup-proxy
-              transition-show="scale"
-              transition-hide="scale"
-            >
-              <q-color v-model="settings.textColor" />
-            </q-popup-proxy>
-          </q-icon>
-        </template>
-      </q-input>
+              <q-icon
+                name="colorize"
+                class="cursor-pointer"
+              >
+                <q-popup-proxy
+                  transition-show="scale"
+                  transition-hide="scale"
+                >
+                  <q-color v-model="temp[con.model]" />
+                </q-popup-proxy>
+              </q-icon>
+            </template>
+          </q-input>
+        </div>
 
-      <div class="text-right">
-        <q-btn
-          dense
-          color="primary"
-          label="Save"
-          class="q-mt-md q-px-md"
-          @click="emitConfig"
-        />
-      </div>
+        <div class="full-width row justify-end">
+          <q-btn
+            round
+            type="submit"
+            icon="check"
+            color="primary"
+          />
+          <q-btn
+            round
+            type="reset"
+            icon="close"
+            color="primary"
+            flat
+            class="q-ml-sm"
+          />
+        </div>
+      </q-form>
     </div>
     <q-btn
       v-else
@@ -80,18 +78,27 @@ export default {
     settings: Object,
     configure: Boolean
   },
+  data () {
+    return {
+      temp: { ...this.settings },
+      conf: [
+        { label: 'Title', model: 'title' },
+        { label: 'Url', model: 'url' },
+        { label: 'Color', colorPicker: true, model: 'color' },
+        { label: 'Text color', colorPicker: true, model: 'textColor' }
+      ]
+    }
+  },
   methods: {
     emitAction () {
       this.$emit('action', 'Clicked')
     },
     emitConfig () {
-      this.debug(this.$refs.input.value)
-      const data = this.$refs.input.value
-      if (data) {
-        const emit = JSON.parse(JSON.stringify(this.settings))
-        emit.title = data
-        this.$emit('config', emit)
-      }
+      const emit = JSON.parse(JSON.stringify(this.temp))
+      this.$emit('config', emit)
+    },
+    reset () {
+      this.temp = { ...this.settings }
     }
   }
 }
