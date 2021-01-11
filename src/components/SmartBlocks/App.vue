53fc30a97c65ac52cc88cdb0ab8b48e2d9dba10a
sonokoh <74166775+sonokoh@users.noreply.github.com>
2021-01-11 14:22:46 -0500

    update: SmartblockApp.vue add: lodash, v-jsoneditor

--- src/components/SmartBlocks/App.vue
+++ src/components/SmartBlocks/App.vue
@@ -2,38 +2,85 @@
   <div>
     <div
       v-if="configure"
-      class="q-pa-sm"
+      class="q-pa-sm q-gutter-y-sm"
     >
-      Choose the app you would like to load...
-      <q-input
+      <div>
+        Choose the app you would like to load.
+      </div>
+      <q-select
+        :loading="!appOptions"
         label="App URL"
-        :value="settings.url"
+        :options="appOptions"
+        option-label="name"
+        option-value="slug"
+        :value="settings.slug"
+        emit-value
+        map-options
+        @input="updateConfig($event, 'slug')"
       />
       <q-input
-        label="Auth Token (optional)"
-        :value="settings.token"
+        label="Minimum Height"
+        :value="settings.style ? settings.style.minHeight : null"
+        @input="updateConfig($event, 'style.minHeight')"
+      />
+      <v-jsoneditor
+        :value="settings.settings"
+        :options="{
+          mode: 'code',
+        }"
+        @input="updateConfig($event, 'settings')"
       />
     </div>
     <div v-else>
-      Your app would go here
+      <iframe
+        :src="url"
+        v-if="url"
+        class="fit q-pa-sm"
+        :style="style"
+        frameBorder="0"
+      />
+      <div v-else>
+        Your app will appear here.
+      </div>
     </div>
   </div>
 </template>
 <script>
+import VJsoneditor from 'v-jsoneditor'
+import set from 'lodash/set'
 export default {
   name: 'SmartBlockApp',
+  components: {
+    VJsoneditor
+  },
   props: {
     settings: Object,
     configure: Boolean
   },
-  methods: {
-    emitAction () {
-      this.$emit('action', 'Clicked')
+  data () {
+    return {
+      appOptions: null
+    }
+  },
+  async created () {
+    const { data } = await this.$api.get('apps', { params: { type: 'Activation' } })
+    this.appOptions = data
+  },
+  computed: {
+    url () {
+      if (!this.settings || !this.settings.slug) return null
+      let url = `https://apps.sourcesync.io/${encodeURIComponent(this.settings.slug)}/`
+      if (this.settings.settings) url += `?settings=${encodeURIComponent(JSON.stringify(this.settings.settings))}`
+      return url
     },
-    emitConfig (data) {
-      const emit = JSON.parse(JSON.stringify(this.settings))
-      emit.image = data
-      this.$emit('config', JSON.stringify(emit))
+    style () {
+      if (!this.settings || !this.settings.style) return {}
+      return this.settings.style
+    }
+  },
+  methods: {
+    updateConfig (value, prop) {
+      this.$emit('config', set({ ...this.settings }, prop, value))
     }
   }
 }
