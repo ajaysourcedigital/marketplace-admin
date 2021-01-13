68d96eedd43ec8fb727fa52cf123fb8c7dab3082
c-49 <reithc4@gmail.com>
2021-01-13 14:00:53 -0600

    Importing Zendesk

---
+++ src/boot/zendesk.js
@@ -0,0 +1,16 @@
+/* eslint-disable */
+import Vue from "vue";
+import Zendesk from "@dansmaculotte/vue-zendesk";
+
+Vue.use(Zendesk, {
+  key: "9917360e-6929-4970-94cf-20c54cf31289",
+  disabled: true,
+  hideOnLoad: true,
+  settings: {
+    webWidget: {
+      color: {
+        theme: "#0D384D"
+      }
+    }
+  }
+});
