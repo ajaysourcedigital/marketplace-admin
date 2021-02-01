2726e936005046856ddbace300b5d5b41ab2987b
sonokoh <74166775+sonokoh@users.noreply.github.com>
2021-02-01 16:41:46 -0500

    add: ad smart block for Mobiquity

--- src/components/SmartBlocks/Ad.vue
+++ src/components/SmartBlocks/Ad.vue
@@ -1,37 +1,135 @@
 <template>
   <div>
-    <div
+    <q-form
       v-if="configure"
-      class="q-pa-sm"
+      @submit="emitConfig"
+      class="q-pa-sm q-gutter-y-sm"
     >
-      Configure this ad by entering the advertizers API and campaign.
-      <q-input label="Provider API" />
-      <q-input label="Campaign ID" />
-      <q-input label="Image URL (optional)" />
+      <div>
+        Configure this ad by entering the advertizers API and campaign.
+      </div>
+      <q-input
+        label="Publisher ID"
+        v-model="internalSettings.advangelists.pubId"
+        required
+      />
+      <q-input
+        label="Width"
+        type="number"
+        v-model="internalSettings.width"
+        required
+      />
+      <q-input
+        label="Height"
+        type="number"
+        v-model="internalSettings.height"
+        required
+      />
+      <q-select
+        label="IAB Categories"
+        :options="catsOpts"
+        :loading="!iabOptions"
+        :disabled="!iabOptions"
+        @filter="catsFilterFn"
+        clearable
+        multiple
+        use-input
+        options-selected-class="text-secondary text-bold"
+        use-chips
+        emit-value
+        map-options
+        v-model="internalSettings.cats"
+      />
+      <q-select
+        label="Blocked IAB Categories"
+        :options="bcatOpts"
+        :loading="!iabOptions"
+        :disabled="!iabOptions"
+        @filter="bcatFilterFn"
+        clearable
+        options-selected-class="text-secondary text-bold"
+        multiple
+        use-chips
+        emit-value
+        use-input
+        map-options
+        v-model="internalSettings.bcat"
+      />
+      <div class="row justify-end">
+        <q-btn
+          color="primary"
+          type="submit"
+          label="Save"
+        />
+      </div>
+    </q-form>
+    <div v-else>
+      <smart-block-ad
+        :settings="internalSettings"
+        :test-mode="true"
+        :error.sync="error"
+        @render="onRender"
+      />
+      <div v-if="error">
+        <q-banner class="text-white bg-red">
+          Sorry, we could not render your ad. Please ensure properties such as "Publisher ID", "Width" and "Height" are correctly entered.
+        </q-banner>
+      </div>
     </div>
-    <img
-      v-else
-      width="100%"
-      :src="settings.image"
-      @click="emitAction"
-    >
   </div>
 </template>
 <script>
+import clonedeep from 'lodash/clonedeep'
+let iabOptions
 export default {
-  name: 'SmartBlockAd',
+  name: 'SmartBlockAdEditor',
+  components: {
+    SmartBlockAd: () => import(/* webpackChunkName: "smart-block-ad" */ './SmartBlockAd.vue')
+  },
   props: {
     settings: Object,
     configure: Boolean
   },
+  data () {
+    return {
+      internalSettings: null,
+      error: null,
+      iabOptions: null,
+      bcatOpts: null,
+      catsOpts: null
+    }
+  },
+  async created () {
+    this.internalSettings = Object.assign({ advangelists: {}, width: 320, height: 250 }, clonedeep(this.settings))
+    if (!iabOptions) {
+      iabOptions = await import('assets/iad_categories.json')
+        .then(module => module.default.map(({ value, label }) => ({ value, label: `[${value}] ${label}` })))
+    }
+    this.iabOptions = iabOptions
+    this.bcatOpts = iabOptions
+  },
   methods: {
-    emitAction () {
-      this.$emit('action', 'Clicked')
+    emitConfig (value, prop) {
+      this.$emit('config', this.internalSettings)
     },
-    emitConfig (data) {
-      const emit = JSON.parse(JSON.stringify(this.settings))
-      emit.image = data
-      this.$emit('config', emit)
+    onRender (args) {
+      this.debug('rendering ad...', args)
+    },
+    filterFn (optName, val, update, abort) {
+      update(() => {
+        if (!val) {
+          this[optName] = iabOptions
+        } else {
+          const needle = val.toLowerCase()
+          this[optName] = iabOptions.filter(v => v.label.toLowerCase().indexOf(needle) > -1 || v.value.toLowerCase().indexOf(needle) > -1)
+        }
+      })
+    },
+    bcatFilterFn (val, update, abort) {
+      this.filterFn('bcatOpts', val, update, abort)
+    },
+    catsFilterFn (val, update, abort) {
+      this.filterFn('catsOpts', val, update, abort)
     }
   }
 }
---
+++ src/components/SmartBlocks/SmartBlockAd.vue
@@ -0,0 +1,84 @@
+<template>
+  <div
+    :style="{
+      width: (settings.width || 0) + 'px',
+      width: (settings.width || 0) + 'px'
+    }"
+    v-html="_adContent"
+    v-if="_adContent"
+  />
+</template>
+
+<script>
+export default {
+  name: 'SmartBlockAd',
+  data () {
+    return {
+      adContent: null
+    }
+  },
+  props: {
+    settings: Object,
+    testMode: Boolean,
+    error: Object
+  },
+  async mounted () {
+    await this.renderContent()
+  },
+  computed: {
+    _adContent () {
+      if (this.adContent) return this.adContent
+      if (this.testMode && !this.error) return 'Your ad will be rendered here.'
+      return null
+    }
+  },
+  methods: {
+    async renderContent () {
+      this.adContent = null
+      this.$emit('update:error', null)
+      if (this.settings.advangelists && this.settings.advangelists.pubId) {
+        const params = new URLSearchParams({
+          fmt: 'html',
+          pubid: this.settings.advangelists.pubId,
+          width: this.settings.width || 0,
+          height: this.settings.height || 0,
+          domain: window.location.origin,
+          page: window.location.origin,
+          appname: window.location.host,
+          secure: 1,
+          // The IAB category that best defines your site
+          cats: this.settings.cats || [],
+          // The IAB category of ads that you do NOT want to see returned
+          bcat: this.settings.bcat || [],
+          dnt: (navigator && navigator.doNotTrack) || 0,
+          displaytype: 'banner',
+          fb: '',
+          isapp: 0
+        })
+        const baseURL = 'https://nep.advangelists.com/xp/js'
+
+        try {
+          const url = baseURL + '?' + params.toString()
+          this.$listeners.render && this.$emit('render', { url })
+          if (process.env.NODE_ENV === 'development' && window.location.hostname === 'localhost') {
+            console.error('This ad do not work in localhost. url: ' + url)
+          } else {
+            const { data: html, status } = await this.$axios.get(url)
+            if (status !== 200) {
+              this.$emit('update:error', { html, status })
+            } else {
+              this.adContent = html
+            }
+          }
+        } catch (e) {
+          console.error('Error occurred while loading the ad.', e)
+        }
+      }
+    }
+  }
+
+}
+</script>
+
+<style>
+</style>
